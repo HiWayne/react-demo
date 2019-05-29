38a43949795156a365a63168f9bf80c5be31d615
LiuTianYu <1211125661@qq.com>
2019-05-29 18:04:01 +0800

    update

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,11 +1,41 @@
 import React from "react"
+import { isArray } from "util";
+import ProductCategoryRow from "./components/ProductCategoryRow"
+import ProductRow from "./components/ProductRow"
 
 export default class ProductTable extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  sortProduct(key) {
+    const data = this.props.data
+    let resultObject = {}
+    if (isArray(data)) {
+      data.forEach(function (object) {
+        const sort = object[key]
+        if (resultObject.hasOwnProperty(sort)) {
+        }
+        else {
+          resultObject[sort] = []
+        }
+        resultObject[sort].push(object)
+      })
+      return resultObject
+    }
+  }
+
   render() {
+    const sortObject = this.sortProduct("category")
+    const category = Object.keys(sortObject).map((category) => {
+      const products = sortObject[category]
+      products = products.map((productData) => <ProductRow key={productData.name} name={productData.name} price={productData.price}/>)
+      return (
+        <ProductCategoryRow key={category} category={category}/>
+        {products}
+      )
+    })
+
     return (
       <div>
         <table>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import FilterableProductTable from "./components/FilterableProductTable"
 import originalData from "../../data.json"
 
+// Array
 const data = JSON.parse(originalData).data
 
 ReactDOM.render(<FilterableProductTable data={data} />, document.querySelector("#root"))
